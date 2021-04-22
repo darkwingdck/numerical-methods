9ba6fcebc772600aaa61525960e87ea3165c6fe1
darkwingdck <dzakharov3819@gmail.com>
2021-04-22 20:53:51 +0300

    Gauss added

--- 2_SOLE.py
+++ 2_SOLE.py
@@ -10,7 +10,7 @@ def gauss(mx):
 			break
 	# I could make a case with null first element in each row, but I won't
    
-   	# Gauss itself (getting triangular matrix)
+   	# getting triangular matrix
 	cur = 0
 	for i in range(len(mx)):
 		for j in range(i + 1, len(mx)):
@@ -18,13 +18,11 @@ def gauss(mx):
 			for k in range(cur, len(mx[i])):
 				mx[j][k] -= mx[i][k] * t
 		cur += 1
-	print(mx)  
-
 
 	# getting answer
 	res = 0
 	s = 0
-	ans = [1, 1, 1]
+	ans = np.ones(len(mx[0]) - 1)
 	curXid = len(ans) - 1
 	for i in range(len(mx) - 1, -1, -1):
 		for j in range(len(mx[i]) - 1):
@@ -34,7 +32,7 @@ def gauss(mx):
 		ans[curXid] = res
 		curXid -= 1
 		s = 0
-	print(ans)
+	return ans
 			
 			
 
@@ -49,10 +47,23 @@ def iteration(mx):
 	pass
 
 def main():
-	mx = np.array([[7, 1, 1, 9], [1, 9, 1, 11], [1, 1, 11, 13]], dtype=float)
+	#mx = np.array([[7, 1, 1, 9], [1, 9, 1, 11], [1, 1, 11, 13]], dtype=float)
 	#mx = np.array([[1, -1, 2, 0], [2, 1, -3, 0], [3, 0, 2, 0]], dtype=float)
+	N = 4
+	a = np.random.uniform(low=3, high=7, size=(N, N))
+	b = np.random.uniform(low=3, high=7, size=(N, 1))
+	mx = np.column_stack((a, b))
+	print("--------------------Gauss--------------------")
+	print("Matrix:")
 	print(mx)
-	gauss(mx)
+	print("\nMy solution:")
+	myGauss = gauss(mx)
+	print(myGauss)
+	print("\nPython solution:")
+	pyGauss = np.linalg.solve(a, b).transpose()
+	print(pyGauss)
+	print("\nError:")
+	print(pyGauss -  myGauss)
 
 
 if __name__ == "__main__":
